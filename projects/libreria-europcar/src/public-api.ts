450f77f2553da69997a7a705e7998667d6fb04b9
Joaquin Colombo <joaquincolombo494@gmail.com>
2023-07-27 02:11:35 +0200

    styles
    creazione e styles de buttons

--- projects/libreria-europcar/src/public-api.ts
+++ projects/libreria-europcar/src/public-api.ts
@@ -5,3 +5,5 @@
 export * from './lib/libreria-europcar.service';
 export * from './lib/libreria-europcar.component';
 export * from './lib/libreria-europcar.module';
+export * from './lib/button/button.component'; // Asegúrate de incluir aquí la exportación de ButtonComponent y cualquier otro componente que quieras exponer desde tu biblioteca.
+
